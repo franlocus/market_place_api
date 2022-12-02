a7a0de2392112d8377c68a9f143ce29167e549e3
franlocus <franciscobuscema@gmail.com>
2022-12-02 14:48:32 -0300

    add create action to users controller

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe 'Api::V1::Users' do
   let!(:user) { create(:user) }
 
-  describe 'GET /show#user' do
+  describe 'GET /users#show' do
     before { get api_v1_user_path(user) }
 
     it { expect(response).to have_http_status(:ok) }
@@ -11,6 +11,25 @@ RSpec.describe 'Api::V1::Users' do
     it 'show user' do
       json_response = JSON.parse(response.body)
       expect(json_response['email']).to eq(user.email)
+    end
+  end
+
+  describe 'POST /users#create' do
+    before do
+      post api_v1_users_path, params: { user: { email: 'buddy@test.org', password: '123456' } }
+    end
+
+    it { expect(response).to have_http_status(:created) }
+
+    it 'creates a user' do
+      json_response = JSON.parse(response.body)
+      expect(json_response['email']).to eq('buddy@test.org')
+    end
+
+    it 'doesnt create a user with taken email' do
+      post api_v1_users_path, params: { user: { email: 'buddy@test.org', password: '123456' } }
+
+      expect(response).to have_http_status(:unprocessable_entity)
     end
   end
 end
