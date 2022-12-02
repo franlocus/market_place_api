5a7aa3ba5bcd67b0d251adae6225f9e04085ffe8
franlocus <franciscobuscema@gmail.com>
2022-12-02 15:56:31 -0300

    Add update action to the users controller

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -32,4 +32,23 @@ RSpec.describe 'Api::V1::Users' do
       expect(response).to have_http_status(:unprocessable_entity)
     end
   end
+
+  describe 'PATCH /users#update' do
+    before do
+      patch api_v1_user_path(user), params: { user: { email: 'happy-buddy@test.org', password: '123456' } }
+    end
+
+    it { expect(response).to have_http_status(:success) }
+
+    it 'updates a user' do
+      json_response = JSON.parse(response.body)
+      expect(json_response['email']).to eq('happy-buddy@test.org')
+    end
+
+    it 'doesnt update a user when invalid params are sent' do
+      post api_v1_users_path, params: { user: { email: 'bad_email', password: '123456' } }
+
+      expect(response).to have_http_status(:unprocessable_entity)
+    end
+  end
 end
