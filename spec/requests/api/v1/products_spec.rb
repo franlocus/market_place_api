a3d4c32174328e85139a23c5fdf120b30afb62dd
franlocus <franciscobuscema@gmail.com>
2022-12-07 10:24:47 -0300

    Add product create, update and destroy actions

--- spec/requests/api/v1/products_spec.rb
+++ spec/requests/api/v1/products_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe 'Api::V1::Products' do
+  let!(:user) { create(:user) }
+  let!(:product) { create(:product, user:) }
+
   describe 'GET /index' do
     before { get api_v1_products_path }
 
@@ -13,8 +16,6 @@ RSpec.describe 'Api::V1::Products' do
   end
 
   describe 'GET /show' do
-    let!(:product) { create(:product) }
-
     before { get api_v1_product_path(product) }
 
     it { expect(response).to have_http_status(:ok) }
@@ -22,6 +23,75 @@ RSpec.describe 'Api::V1::Products' do
     it 'returns show product' do
       json_response = JSON.parse(response.body)
       expect(json_response['title']).to eq(product.title)
+    end
+  end
+
+  describe 'POST /create' do
+    context 'with authorization' do
+      before do
+        post api_v1_products_path,
+             params: { product: { title: product.title, price: product.price, published: product.published } },
+             headers: { Authorization: JsonWebToken.encode(user_id: product.user_id) }
+      end
+
+      it { expect(response).to have_http_status(:created) }
+
+      it { expect(Product.last.title).to eq(product.title) }
+    end
+
+    context 'without authorization' do
+      before do
+        post api_v1_products_path,
+             params: { product: { title: product.title, price: product.price, published: product.published } }
+      end
+
+      it { expect(response).to have_http_status(:forbidden) }
+    end
+  end
+
+  describe 'PATCH /update' do
+    context 'with authorization' do
+      before do
+        patch api_v1_product_path(product),
+              params: { product: { title: "#{product.title} new" } },
+              headers: { Authorization: JsonWebToken.encode(user_id: product.user_id) }
+      end
+
+      it { expect(response).to have_http_status(:success) }
+
+      it { expect(Product.last.title).to eq("#{product.title} new") }
+    end
+
+    context 'when signed in but without being product owner' do
+      # Another user cannot modify first user's product
+      before do
+        patch api_v1_product_path(product),
+              params: { product: { title: "#{product.title} new" } },
+              headers: { Authorization: JsonWebToken.encode(user_id: create(:user).id) }
+      end
+
+      it { expect(response).to have_http_status(:forbidden) }
+    end
+  end
+
+  describe 'DELETE /destroy' do
+    context 'with authorization' do
+      before do
+        delete api_v1_product_path(product), headers: { Authorization: JsonWebToken.encode(user_id: product.user_id) }
+      end
+
+      it { expect(response).to have_http_status(:no_content) }
+
+      it { expect { product.reload }.to raise_error(ActiveRecord::RecordNotFound) }
+    end
+
+    context 'when signed in but without being product owner' do
+      # Another user cannot delete first user's product
+      before do
+        delete api_v1_product_path(product), headers: { Authorization: JsonWebToken.encode(user_id: create(:user).id) }
+      end
+
+      it { expect(response).to have_http_status(:forbidden) }
     end
   end
 end
