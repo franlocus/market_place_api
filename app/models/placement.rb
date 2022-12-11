a8702ccf61ebcf1c25fde32c4aab85031e2f68ec
franlocus <franciscobuscema@gmail.com>
2022-12-11 11:55:10 -0300

    fix placement validation causing error loop

--- app/models/placement.rb
+++ app/models/placement.rb
@@ -1,7 +1,4 @@
 class Placement < ApplicationRecord
-  validates :order_id, presence: true
-  validates :product_id, presence: true
-
   belongs_to :order
   belongs_to :product, inverse_of: :placements
 end
--- spec/models/placement_spec.rb
+++ spec/models/placement_spec.rb
@@ -1,9 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Placement do
-  it { is_expected.to validate_presence_of(:order_id) }
-  it { is_expected.to validate_presence_of(:product_id) }
-
   it { is_expected.to belong_to(:order) }
   it { is_expected.to belong_to(:product).inverse_of(:placements) }
 end
