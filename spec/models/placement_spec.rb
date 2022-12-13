19600b2860bde1ff8d075dbff4f8d63d0d76b06a
franlocus <franciscobuscema@gmail.com>
2022-12-13 20:38:38 -0300

    Decreases the product quantity by the placement quantity

--- spec/models/placement_spec.rb
+++ spec/models/placement_spec.rb
@@ -1,6 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Placement do
+  let!(:placement) { create(:placement) }
+
   it { is_expected.to belong_to(:order) }
   it { is_expected.to belong_to(:product).inverse_of(:placements) }
+
+  context 'when creating a placement' do
+    it 'decreases the product quantity by the placement quantity' do
+      product = placement.product
+      expect { placement.decrement_product_quantity! }.to change(product, :quantity).by(-placement.quantity)
+    end
+  end
 end
