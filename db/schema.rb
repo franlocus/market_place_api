a56ca8a41a88a90cb42d2b7f0e5df50a7a709c47
franlocus <franciscobuscema@gmail.com>
2022-12-13 20:28:05 -0300

    Add quantity to placements

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_12_195522) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_13_232413) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,6 +27,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_12_195522) do
     t.bigint "product_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "quantity", default: 0
     t.index ["order_id"], name: "index_placements_on_order_id"
     t.index ["product_id"], name: "index_placements_on_product_id"
   end
--- spec/factories/placements.rb
+++ spec/factories/placements.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :placement do
+    quantity { 5 }
     order
     product
   end
