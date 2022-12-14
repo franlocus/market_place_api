21906b5b05da6e23b4dbb728c4fda448dda27209
franlocus <franciscobuscema@gmail.com>
2022-12-14 17:10:49 -0300

    Add caching for the serializers

--- app/serializers/order_serializer.rb
+++ app/serializers/order_serializer.rb
@@ -1,5 +1,7 @@
 class OrderSerializer
   include JSONAPI::Serializer
+  cache_options store: Rails.cache, namespace: 'jsonapi-serializer', expires_in: 1.hour
+
   belongs_to :user
   has_many :products
 end
--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,5 +1,7 @@
 class ProductSerializer
   include JSONAPI::Serializer
+  cache_options store: Rails.cache, namespace: 'jsonapi-serializer', expires_in: 1.hour
+
   attributes :title, :price, :published
   belongs_to :user
 end
